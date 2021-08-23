9e373220ed2e037da25ec5ee157845f5a8214f84
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2021-08-23 18:58:18 +0200

    Корректирует путь в ссылках pnotify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
-import { alert, defaultModules } from '..node_modules/@pnotify/core/dist/PNotify.js';
+import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
 
 alert({
     text: 'Notice me, senpai!'
-  });
+});
+  
