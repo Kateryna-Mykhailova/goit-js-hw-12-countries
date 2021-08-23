3223a17e4b834504ad1067d8c8bcb055cfec9f21
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2021-08-23 23:29:14 +0200

    Устанавливает lodash.debounce

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import './sass/main.scss';
 import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import debounce from 'lodash.debounce';
 
-alert({
-    text: 'Notice me, senpai!'
-});
+
+// alert({
+//     text: 'Notice me, senpai!'
+// });
   
+console.log(debounce
+);
+
+
+// debounce(test, 2000);
+
+// function test() {
+//     console.log("вап");
+//  }
+
+
