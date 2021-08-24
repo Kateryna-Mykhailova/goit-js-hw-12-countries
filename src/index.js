4a7ef665a7d743bb5d1df9adc5c9d985c6ab4519
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2021-08-24 23:38:04 +0200

    Добавляет функцию для рендора разметки списка стран

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './sass/main.scss';
 import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import debounce from 'lodash.debounce';
 
+
 const refs = {
     form: document.querySelector('#form'),
     input: document.querySelector('#search'),
@@ -11,7 +12,8 @@ const refs = {
 refs.input.addEventListener('input', debounce(onTextInput, 500));
 
 function onTextInput(e) {
-    e.preventDefault()
+    e.preventDefault();
+    resetSearch();
     const name = e.target.value
     // console.log(e.target.value);
     fetch(`https://restcountries.eu/rest/v2/name/${name}`)
@@ -19,37 +21,96 @@ function onTextInput(e) {
         .then(data => renderCollection(data))
         // .then(data => console.log(data))
         .catch(err => console.log(err))
+   
 };
 
-function createElement({ name, population, flag, capital, languages}) {
-    // const language = 
-    const article = `
+function createCountry({ name, population, flag, capital, languages }) {
+     resetSearch();
+    const language = languages.map(el => `<li>${el.name}</li>`).join(' ')
+  
+    const country = `
     <article>
   <h1>${name}</h1>
   <img src="${flag}" alt="">
   <p>${capital}</p>
   <p>${population}</p>
-  <ul>${languages.name}</ul>
+  <ul>${language}</ul>
 </article>
 `
-    refs.container.insertAdjacentHTML('beforeBegin', article)
+    refs.container.insertAdjacentHTML('beforeBegin', country)
  }
 
 
+function createCountries({ name }) {
+     resetSearch();
+    const countriesList = `
+    <ul>
+  <li>${name}</li>
+</ul>
+    `
+    refs.container.insertAdjacentHTML('beforeBegin', countriesList)
+ }
+
+function resetSearch() {
+    refs.container.innerHTML = '';
+}
 // function leng(languages) {
 //      arr.forEach(el => createElement(el))
 // };
  
+// изначальная функция
+// function renderCollection(arr) {
+//     arr.forEach(el => createCountry(el))
+// }
 function renderCollection(arr) {
-    arr.forEach(el => createElement(el))
+      resetSearch()
+    if (arr.length === 1) {
+       
+        arr.forEach(el => createCountry(el))
+        return
+    }
+    else if (arr.length <= 10 && arr.length >= 2) {
+       
+        arr.forEach(el => createCountries(el))
+        return
+    }
+    else{ alert({text: 'Too many matches found. Please enter a more specific query!'}) }
+   
 }
 
 
 
+// function renderCollection(arr) {
+
+//     arr.forEach(el => createCountry(el))
+    
+    
+    // {
+
+    //     if (arr.length === 1) {  }
+
+
+    //     // if (arr.length > 10{
+    //  
+    //     // ) 
+
+    //     // if (arr.length <= 10) {
+    //     //     createCountries(el)
+    //     // }
+    //     // else { }
+
+    // } )
+// }
 
 
 
 
+
+
+// я делал шаблонной строкой и там где массив просто писал стрелку через map()
+// <ul class="languages_list">Languages: 
+//                 ${languages.map(value => `<li class="language">${value.name}</li>`).join(" ")}
+//               </ul> 
 
 
 
@@ -63,23 +124,5 @@ function renderCollection(arr) {
 // fetch(`https://restcountries.eu/rest/v2/name/{name} = ${value}`)
 
 //
-
-
-// function onTextInputS() {
-//     console.log(refs.input.value);
-// }
-// alert({
-//     text: 'Notice me, senpai!'
-// });
-  
-// console.log(debounce);
-
-//https://restcountries.eu/rest/v2/name/{name}
-
-// debounce(test, 2000);
-
-// function test() {
-//     console.log("вап");
-//  }
 
 
